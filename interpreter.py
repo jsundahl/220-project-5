46cc543870e0586ff25bc11938924d3a505b2f6a
jsundahl <jsundahl15@gmail.com>
2017-04-03 17:43:44 -0400

    stub helper functions for eval_node

--- interpreter.py
+++ interpreter.py
@@ -3,6 +3,7 @@ from env import GlobalEnv, LocalEnv
 genv = GlobalEnv.empty_env()
 result = 0
 
+
 def eval_tree(tree):
     """ The top level function.
         Args:
@@ -15,8 +16,10 @@ def eval_tree(tree):
     # Here, get the list of children nodes. Iterate over that list, calling eval_node on each node.
     return result
 
+
 def node_name(node):
     return type(node).__name__
+
 
 def eval_node(node, env):
     """ Evaluates a Node object in the abstract syntax tree.
@@ -33,30 +36,67 @@ def eval_node(node, env):
     if node_type == 'Expr':
         return eval_node(node.value, env)
     elif node_type == 'Assign':
-        # extract the variable name, evaluate the RHS, then extend the environment.
-        return 0, env
+        # extract the variable name, evaluate the right hand side, then extend the environment.
+        return assign(node, env)
     elif node_type == 'BinOp':
         # get the left and right operands (we use only single operands) and the operator.
         # evaluate the operands and apply the operator. return the number, env.
-        return 0, env
+        return bin_op(node, env)
     elif node_type == 'FunctionDef':
         # need the function id (name), args, and body. Extend the environment.
         # you can leave the args wrapped in the ast class and the body and unpack them
         # when the function is called.
-        return 0, env
+        return fundtion_def(node, env)
     elif node_type == 'Call':
         # get any values passed in to the function from the Call object.
         # get the fxn name and look up its parameters, if any, and body from the env.
         # get lists for parameter names and values and extend a LocalEnv with those bindings.
         # evaluate the body in the local env, return the value, env.
-        return 0, env
+        return call(node, env)
     elif node_type == 'Return':
         # evaluate the node, return the value, env.
-        return 0, env
+        return retrn(node, env)
     elif node_type == 'Name':
         # Name(identifier id)- lookup the value binding in the env
         # return the value, env
-        return 0, env
-    # Num(object n) -- a number, return the number, env.
+        return name(node, env)
     elif node_type == 'Num':
+        # Num(object n) -- a number, return the number, env.
         return node.n, env
+
+
+def assign(node, env):
+    # extract the variable name, evaluate the right hand side, then extend the environment.
+    raise NotImplementedError
+
+
+def bin_op(node, env):
+    # get the left and right operands (we use only single operands) and the operator.
+    # evaluate the operands and apply the operator. return the number, env.
+    raise NotImplementedError
+
+
+def fundtion_def(node, env):
+    # need the function id (name), args, and body. Extend the environment.
+    # you can leave the args wrapped in the ast class and the body and unpack them
+    # when the function is called.
+    raise NotImplementedError
+
+
+def call(node, env):
+    # get any values passed in to the function from the Call object.
+    # get the fxn name and look up its parameters, if any, and body from the env.
+    # get lists for parameter names and values and extend a LocalEnv with those bindings.
+    # evaluate the body in the local env, return the value, env.
+    raise NotImplementedError
+
+
+def retrn(node, env):
+    # evaluate the node, return the value, env.
+    raise NotImplementedError
+
+
+def name(node, env):
+    # Name(identifier id)- lookup the value binding in the env
+    # return the value, env
+    raise NotImplementedError
